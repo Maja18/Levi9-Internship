b7bcdc012ec46beb07485997d4141cddfca0906c
Maja18 <maja98dragojlovic@gmail.com>
2022-06-13 13:38:33 +0200

    [Feature] creating initial project

---
+++ SocialNetworking/SocialNetworking/src/test/java/Internship/SocialNetworking/SocialNetworkingApplicationTests.java
@@ -0,0 +1,13 @@
+package Internship.SocialNetworking;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SocialNetworkingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
