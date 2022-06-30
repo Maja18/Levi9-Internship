d00122f4fc0040cd3260d0f98cb7281b625c0470
Milinovic99 <milos_milinovic051@uns.ac.rs>
2022-06-30 20:29:51 +0200

    [Feature] fixing test repositories

--- SocialNetworking/SocialNetworking/src/test/java/Internship/SocialNetworking/repository/PostRepositoryTests.java
+++ SocialNetworking/SocialNetworking/src/test/java/Internship/SocialNetworking/repository/PostRepositoryTests.java
@@ -1,7 +1,6 @@
 package Internship.SocialNetworking.repository;
 import Internship.SocialNetworking.models.Post;
 import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.transaction.annotation.Transactional;
 
